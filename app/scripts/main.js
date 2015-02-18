be4811db695e607900acd035351372d3dd8858df
Eric Adlam <eadlam@gmail.com>
2015-02-18 19:07:09 +1030

    removing demo stuff, adding bower package info

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,208 +1,45 @@
-var f1 = function(n){                   
-    for(var i = n; i > 0; i--){  
-        if(true){
-            console.log("Even!")
-        }       
-        console.log("[f1] Count: ", i); 
-    }};
-var f1p = function(n){   
-    var status = n - 2;                
-    for(var i = n; i > 0; i--){  
-        var x = 3;
-        if(status){
-            console.log("Even!");
-            while(x > i){
-                console.log("Status:", status);
-               x--; 
-            }
-        }       
-        console.log("[f1] Count: ", i); 
-    }};
-var s1 = "var f1 = " + f1.toString() ;
-var s1p = "var f1p = " + f1p.toString() ;
+var template = syntactic.outline(s1, {
+    ignore:['VariableDeclarator', 
+            'BlockStatement', 
+            'Identifier', 
+            'Literal', 
+            'Line',
+            'ExpressionStatement']}
+);
 
-var f2 = function(n){
-    while(n > 0){
-        console.log("[f2] Count: ", n);
-        n--;
-    }
-};
+var template = new SerializedStructure(s1);
+console.log(template.verify(s1p));
 
-var f3 = function(n){
-    if(n > 0){
-        console.log("[f3] Count: ", n);
-        f3(n - 1);
-    }
-};
+syntactic.tokenize(s1).then(function(summary){
+    console.log(summary);
+});
 
-var s1 = "var f1 = function(n){         \n"+                  
-    "for(var i = n; i > 0; i--){        \n"+ 
-        "if(i%2 === 0){console.log('even!')}; \n" +     
-        "console.log('[f1] Count: ', i); \n"+
-    "}                                   \n"+
-"};                                      \n"
-
-
-
-
-function Bag(){
-    this._object = {};
-    this._summary = [];
-    this._exclude = ['VariableDeclarator', 'BlockStatement', 'Identifier', 'Literal', 'ExpressionStatement'];
-}
-Bag.prototype.collect = function(branches){
-    var self = this;
-    var _collect = function(branches, branch){
-        var names = Object.keys(branches);
-        if(branches.hasOwnProperty('type')){
-            branch[branches.type] = {};
-            var branch = branch[branches.type];
-        }
-        for(var i in branches){
-            if(branches.hasOwnProperty(i)){
-                if(branches[i] && typeof branches[i] === 'object'){
-                    _collect(branches[i], branch);
-                }
-            }
-        }
-    };
-    var _summarize = function(branch, summary){
-        if(Object.keys(branch).length === 0){
-            self._summary.push(summary);
-        } else {
-            for (var name in branch) {
-                if(branch.hasOwnProperty(name)){
-                    var summaryName = self._exclude.indexOf(name) > -1 ? "" : name;
-                    var newSummary = summary === "" ? summaryName : summaryName === "" ? summary : summary + "." + summaryName;
-                    _summarize(branch[name], newSummary);
-                }
-            };
-        }
-    };
-    _collect(branches, self._object);
-    _summarize(self._object, "");
-}
-Bag.prototype.contains = function(template){
-    var self = this;
-    var info = {
-        status: true,
-        missing:[]
-    };
-    for (var i = template._summary.length - 1; i >= 0; --i) {
-        if(self._summary.indexOf(template._summary[i]) === -1){
-            info.status = false;
-            info.missing.push(template._summary[i]);
-        }
-    };
-    return info;
-
-}
-
-var template = new Bag();
-// template.collect(esprima.parse(s1));
-
-var bag = new Bag();
-bag.collect(esprima.parse(s1p));
-
-// console.log("Bag 1: ", template);
-// console.log("Bag 2: ", bag);
-// console.log("Compare: ", bag.contains(template));
-
-// This converts the list of tokens into a hashmap in the style:
-// {TokenType:{TokenValue:Count}}
-var summarize = function(text){
-    var dict = {};
-    var tokens = esprima.tokenize(text); 
-    for(var i = 0; i < tokens.length; ++i){
-        var token = tokens[i];
-        if( token.type in dict){
-            if(token.value in dict[token.type]){
-                dict[token.type][token.value] += 1;
-            } else {
-                dict[token.type][token.value] = 1;
-            }
-        } else {
-            dict[token.type] = {};
-            dict[token.type][token.value] = 1;
-        }
-    }
-    return dict;
-}
-
-var verify = function(text, options){
-
-    var summary = summarize(text);
-
-    var info = {
-        status:true,   // Assume the text conforms
-        whitelist: {}, // items it's missing
-        blacklist: {}  // items it includes (but shouldn't)
-    };
-    var _inSummary = function(type, value){
-        var result = false;
-        if(summary[type] && summary[type][value]){
-            result = true;
-        } 
-        return result;
-    };
-    var _traverse = function(list, fn){
-        var types = Object.keys(list);
-        for (var i = types.length - 1; i >= 0; --i) {
-            var types = Object.keys(list);
-            var type = types[i];
-            var values = list[type];
-            for (var j = values.length - 1; j >= 0; --j) { 
-                var value = values[j];
-                fn(type,value);
-            }
-        }
-    };
-    var _push = function(list, type, value){
-        if(!list[type]){
-            list[type] = [value];
-        } else {
-            list[type].push(value);
-        }
-    };
-
-    if(options){ // catch missing options
-        if(options.whitelist){ 
-           _traverse(options.whitelist, function(type, value){
-                if(!summary[type] || !summary[type][value]){
-                    info.status = false;
-                    _push(info.whitelist, type, value);
-                }
-           }); 
-        }
-        if(options.blacklist){ 
-           _traverse(options.blacklist, function(type, value){
-                if(summary[type] && summary[type][value]){
-                    info.status = false;
-                    _push(info.blacklist, type, value);
-                }
-           }); 
-        } 
-        if(!options.blacklist && !options.whitelist){
-            throw "this method requires options [whitelist and/or blacklist]"
-        }
-    } else {
-        throw "this method requires options [whitelist and/or blacklist]"
-    }
-    // console.log(info);
-    return info;
-};
-
-var dict = summarize(s1);
-var info = verify(s1, {
-    whitelist: {
-        Keyword:['for'],
-        Keyword:['var'],
-    },
-    blacklist: {
-        Keyword:['while']
-
+var spec = syntactic.specify({
+    blacklist:{
+        Keyword:['for', 'while']
     }
 });
-// console.log(dict);
-// console.log(info);
-// console.log(esprima.parse(s1));
+spec.verify(s1p).then(function(res){ 
+    console.log(res);
+});
+
+console.log(syntactic.parse(s1p));
+
+
+
+
+for (var name in branch) {
+                if(branch.hasOwnProperty(name)){
+                    var summaryName = this.ignore && 
+                                      this.ignore.indexOf(name) > -1 ? "" : name;
+                    var newSummary = "";
+                    if(summary === ""){
+                        newSummary = summaryName;
+                    } else if (summaryName === ""){
+                        newSummary = summary;
+                    } else {
+                        newSummary = summary + "." + summaryName;
+                    }
+                    this._serialize(branch[name], newSummary);
+                }
+            };
--- syntactic.js
+++ syntactic.js
@@ -1,19 +1,31 @@
-"use strict";
+'use strict';
 
 (function(global) {
 
+
     /* syntactic.js provides an API for several operations over an abstract 
      * syntax tree that can be useful for automated testing of a students 
-     * javascript code 
+     * javascript code. 
      * 
      * Dependencies: esprima.js, q.js
+     *
      */
-
 
     var syntactic = {};
 
     global.syntactic = syntactic;
 
+
+    // When simplifying the AST, removing these nodes tends to make branches
+    // shorter while still preserving the overall structure
+    var REDUNDANT_TOKENS = [
+        'VariableDeclarator', 
+        'BlockStatement', 
+        'Identifier', 
+        'Literal', 
+        'Line',
+        'ExpressionStatement'
+    ];
 
 
     // Modifications of Esprima methods
@@ -26,33 +38,43 @@
     // 2. It removes any node that doesn't have a type attribute
     // 3. It renames the node to the value of it's type attribute
     syntactic.parse = function(text){
-        var structure = {};
+        var bag = {};
         var comments = [];
-        var _parse = function(branches, stem){
-            if(branches.hasOwnProperty('type')){
-                if(branches.type === 'Line'){
-                    comments[branches.loc.end.line + 1] = branches.value;
+
+        // Recursively walk through the ast
+        var _parse = function(branch, bag){
+
+            // collect the node if it has a type attribute
+            if(branch.hasOwnProperty('type')){
+
+                // Capture comments to be used as hints
+                if(branch.type === 'Line'){
+                    comments[branch.loc.end.line + 1] = branch.value;
                 } else {
-                    var lineNo = branches.loc ? branches.loc.start.line : undefined;
-                    stem[branches.type] = {line:lineNo};
-                    var stem = stem[branches.type];
+                    // Collect the node
+                    var lineNo = branch.loc ? branch.loc.start.line : undefined;
+                    bag[branch.type] = {line:lineNo};
+                    bag = bag[branch.type];
                 }
             }
-            for(var i in branches){
-                if(branches.hasOwnProperty(i)){
-                    if(branches[i] && typeof branches[i] === 'object'){
-                        _parse(branches[i], stem);
+
+            // repeat the above for any child objects of this node.
+            // ignore child attributes, they are just metadata
+            for(var i in branch){
+                if(branch.hasOwnProperty(i)){
+                    if(branch[i] && typeof branch[i] === 'object'){
+                        _parse(branch[i], bag);
                     }
                 }
             }
         };
         var ast = esprima.parse(text, {loc:true, comment:true});
-        console.log(ast);
-        _parse(ast, structure);
-        return {structure: structure, comments:comments};
+        _parse(ast, bag);
+        return {structure: bag, comments:comments};
     };
 
 
+    // Asynchronous
     // This method returns the esprima tokens list as a map
     // {Keyword:{for:[ {end: {column:7, line:2}, start: {column:4, line:2}}]}}
     syntactic.tokenize = function(text){
@@ -84,7 +106,7 @@
     // Whitelist / Blacklist Specification
     // ===================================
 
-    // This object is composed of
+    // This Requirements object is composed of
     //     1. the whitelist/blacklist
     //     2. a "flags" object which contains copies of those items in the 
     //        whitelist/blacklist which were not conformed to
@@ -94,7 +116,9 @@
         this.specify(options);
         this.flags = {};
         this.status = true;
-    };
+    }
+
+    // Steps through the whitelist/blacklist and applies the provided function
     Requirements.prototype._traverse = function(list, fn){
         var tokenTypes = Object.keys(list);
         for (var i = tokenTypes.length - 1; i >= 0; --i) {
@@ -104,6 +128,8 @@
             }
         }
     };
+
+    // This method is for flagging a missing/offending token type
     Requirements.prototype._flag = function(listType, type, value, locs){
         this.status = false;
         if(!this.flags[listType]){
@@ -114,6 +140,9 @@
         }
         this.flags[listType][type][value] = locs;
     };
+
+    // This instantiates the object with a whitelist and/or blacklist which
+    // will remain cached in the object
     Requirements.prototype.specify = function(options){
         if(options.hasOwnProperty('whitelist')){
             this.whitelist = options.whitelist;
@@ -121,13 +150,21 @@
         if(options.hasOwnProperty('blacklist')){
             this.blacklist = options.blacklist;
         }
-    }
+    };
+
     // Asynchronous
+    // accepts a block of text which it tokenizes and compares to it's
+    // whitelist / blacklist. 
     Requirements.prototype.verify = function(text){
         var self = this;
         
         var deferred = Q.defer();
-        
+
+        // Reset object state
+        this.flags = {};
+        this.status = true;
+
+        // Verify
         syntactic.tokenize(text).then(function(summary){
             if(self.whitelist){
                 self._traverse(self.whitelist, function(type, value){     
@@ -158,10 +195,9 @@
     // Comparing code against a template structure 
     // ===========================================
 
-    // This object parses text using the syntactic.parse method and then 
+    // This class parses text using the syntactic.parse method and then 
     // serializes the resulting object into string representations of each 
-    // branch. You can specifify nodes to ignore and compare the object to 
-    // another object.
+    // branch.
     function SerializedStructure(text, options){
         var parsed = syntactic.parse(text);
         this.structure = parsed.structure;
@@ -170,24 +206,26 @@
         this.serialized = {};
 
         // do the serialization
-        this._serialize(this.structure, "");
-    };
+        this._serialize(this.structure, '');
+    }
+
     SerializedStructure.prototype._serialize = function(branch, branchName){
         var stems = Object.keys(branch);
         for (var i = stems.length - 1; i >= 0; --i) {
             var stemName = stems[i];
-            if(stemName !== 'line'){ // ignore line numbers, they aren't structure
+
+            // ignore line numbers, they aren't structure
+            if(stemName !== 'line'){ 
                 var name = branchName;
                 if(this.ignore.indexOf(stemName) === -1){
-                    serial = branchName === "" ? stemName : 
-                                                 branchName + "." + stemName;
+                    name = branchName === '' ? stemName : 
+                                               branchName + '.' + stemName;
                 }
-                // end of the branch
+                // end of the branch, store serialized branch name
                 if(Object.keys(branch[stemName]).length === 1){ 
 
-                    if(stemName === 'Line'){ 
-                        console.log(branch[stemName]);
-                    } else {
+                    // ignore comments, they aren't structure
+                    if(stemName !== 'Line'){
                         var line = branch[stemName].line;
                         if(! this.serialized.hasOwnProperty(name)){ 
                             this.serialized[name] = {};
@@ -196,10 +234,11 @@
                         
                     }
                 } else {
-                    this._serialize(branch[stemName], serial);
+                    // recurse
+                    this._serialize(branch[stemName], name);
                 }
             }
-        };
+        }
     };
 
     // Checks to see if a new block of codes matches the structure of this one.
@@ -234,21 +273,25 @@
                 for (var i in lines) {
                     var hint = lines[i];
                     if(hint){
-                        info.hints.push(hint);
+                        info.hints[i] = hint;
                     }
-                };
+                }
             }
-        };
+        }
 
         return info;
     };
 
 
     // Public API method
-    syntactic.outline = function(example, options){
+    syntactic.outline = function(example){
+
+        // This could be a public argument, but probably doesn't need to be
+        var options = {ignore:REDUNDANT_TOKENS};
+
         return new SerializedStructure(example, options);
     };
 
 
-})(typeof window !== "undefined" ? window : global);
+})(typeof window !== 'undefined' ? window : global);
 
